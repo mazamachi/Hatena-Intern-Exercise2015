a0162aace44cc12e4e305026c7bef20d2143315b
Shota Miyazaki <jmsf0203@gmail.com>
2015-08-02 21:39:07 +0900

    perl3: added group_by_user

--- perl/lib/LogCounter.pm
+++ perl/lib/LogCounter.pm
@@ -8,6 +8,17 @@ sub new {
 };
 
 sub group_by_user {
+	my ($self, @args) = @_;
+	my %h;
+	foreach my $log (@{$self->{logs}}) {
+		my $user_name = ($log->{user}) ? $log->{user} : "guest";
+		if($h{$user_name}){
+			push $h{$user_name}, $log;
+		}else{
+			$h{$user_name} = [$log];
+		}
+	}
+	return \%h;
 }
 
 sub count_error {
--- perl/main3.pl
+++ perl/main3.pl
@@ -9,4 +9,5 @@ use LogCounter;
 my $parser = Parser->new( filename => '../sample_data/log.ltsv' );
 my $counter = LogCounter->new($parser->parse);
 print 'total error size: ' . $counter->count_error . "\n";
+$counter->group_by_user;
 print Dumper $counter->group_by_user;
