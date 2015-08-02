423b7073029a281d906f7f96dfa9e482e3b546a9
Shota Miyazaki <jmsf0203@gmail.com>
2015-08-02 22:14:22 +0900

    js1: created parseLTSVLog

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
-
+function parseLTSVLog(str) {
+  var reg = /([^:]+):(.+)\t([^:]+):(.+)\n/g
+  var array=[];
+  var ar;
+  while(ar = reg.exec(str)){
+    var hash = {};
+    hash[ar[1]]= ar[2];
+    hash[ar[3]]= ar[4];
+    array.push(hash);
+  }
+  return array;
+}
 // 課題 JS-2: 関数 `createLogTable` を記述してください
