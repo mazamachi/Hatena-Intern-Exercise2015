6900200b55942c909c76df3e3531448e15622c77
Shota Miyazaki <jmsf0203@gmail.com>
2015-08-07 17:18:38 +0900

    js2: answered

--- js/main.js
+++ js/main.js
@@ -12,3 +12,31 @@ function parseLTSVLog(str) {
   return array;
 }
 // 課題 JS-2: 関数 `createLogTable` を記述してください
+function createLogTable (dom, ltsvLog) {
+  var table = document.createElement("table");
+  dom.appendChild(table);
+
+  var thead = document.createElement("thead");
+  table.appendChild(thead);
+
+  var thRow = thead.insertRow(-1);
+
+  var thPath = document.createElement("th");
+  thPath.innerHTML = "path"
+  thRow.appendChild(thPath);
+
+  var thEpoch = document.createElement("th");
+  thEpoch.innerHTML = "epoch"
+  thRow.appendChild(thEpoch);
+
+  var tbody = document.createElement("tbody");
+  table.appendChild(tbody);
+
+  for (i=0; i<ltsvLog.length; i++) {
+    var rows = tbody.insertRow(-1);
+    var pathCell = rows.insertCell(-1);
+    pathCell.innerHTML = ltsvLog[i].path;
+    var epochCell = rows.insertCell(-1);
+    epochCell.innerHTML = ltsvLog[i].epoch;
+  }
+}
