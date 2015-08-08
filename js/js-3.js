921ab7fa844f78473873f2f9bc962872d9826c5d
Shota Miyazaki <jmsf0203@gmail.com>
2015-08-08 17:12:30 +0900

    js3: created js^3.js

--- js/js-3.js
+++ js/js-3.js
@@ -1,1 +1,7 @@
 // 課題 JS-3 の実装をここに記述してください。
+var button = document.getElementById('submit-button');
+button.addEventListener( "click" , function() {
+	var logInput = document.getElementById('log-input');
+	var logOutput = document.getElementById('table-container');
+	createLogTable(logOutput, parseLTSVLog(logInput.value));
+} , false );
