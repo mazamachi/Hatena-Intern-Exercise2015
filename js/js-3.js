9dbba2c802950955f8ba3a3bf085628facffb22c
Shota Miyazaki <jmsf0203@gmail.com>
2015-08-08 22:15:43 +0900

    js3: fixed to delete table before create log table

--- js/js-3.js
+++ js/js-3.js
@@ -3,5 +3,6 @@ var button = document.getElementById('submit-button');
 button.addEventListener( "click" , function() {
 	var logInput = document.getElementById('log-input');
 	var logOutput = document.getElementById('table-container');
+	logOutput.innerHTML = "";
 	createLogTable(logOutput, parseLTSVLog(logInput.value));
 } , false );
